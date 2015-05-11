56b2cd3e182716956c82245d25c81fd606a80284
David Zukowski <zukoweb@gmail.com>
2015-05-11 10:41:58 -0400

    fix(webpack) removed template string from webpack make file since nodejs does not yet support it out of the box.

--- build/webpack/make.js
+++ build/webpack/make.js
@@ -5,5 +5,5 @@ const merge = require('../../utils/merge-deep');
 // Return default configuration extended based on current environment
 module.exports = exports = merge(
   require('./configs/default'),
-  require(`./configs/${env}`)
+  require('./configs/' + env)
 );
