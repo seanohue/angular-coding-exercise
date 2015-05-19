519933ee5ff681c032686d80516822867dbe30e3
David Zukowski <zukoweb@gmail.com>
2015-05-19 15:07:10 -0400

    feat(webpack) webpack config can now be overrided using "make". Css is now only bundled to disk when webpack is not run in hot mode.

--- build/webpack/configs/production.js
+++ build/webpack/configs/production.js
@@ -1,12 +1,8 @@
 const config  = require('./default');
 const webpack = require('webpack');
-const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 module.exports = exports = {
   plugins : config.plugins.concat([
-    new ngAnnotatePlugin({
-      add: true
-    }),
     new webpack.optimize.UglifyJsPlugin({
       output : {
         'comments'  : false
