f75b0c195193c681baab01b7b9a75686c23c84b4
David Zukowski <zukoweb@gmail.com>
2015-06-05 11:17:39 -0400

    feat(ng-annotate) ng-annotate-loader now runs during production compilation

--- build/webpack/configs/production.js
+++ build/webpack/configs/production.js
@@ -16,10 +16,16 @@ module.exports = exports = {
     new ExtractTextPlugin('[name].[contenthash].css')
   ]),
   loaders : config.module.loaders.map(loader => {
+
+    // compile css to a separate file
     if (/\.scss/.test(loader.test)) {
       loader.loader = ExtractTextPlugin.extract(
         loader.loaders[0], loader.loaders.slice(1).join('!')
       );
+    }
+    // run ng-annotate
+    else if (/\.js/.test(loader.test)) {
+      loader.loaders.unshift('ng-annotate');
     }
 
     return loader;
